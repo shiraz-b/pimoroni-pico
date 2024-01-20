36a3d0d069d853b378b47bdde318142d58de6f2b
shiraz-b <23420634+shiraz-b@users.noreply.github.com>
2024-01-20 10:51:56 +0000

    shiraz walk testing

--- micropython/examples/plasma_stick/shiraz_walk.py
+++ micropython/examples/plasma_stick/shiraz_walk.py
@@ -10,9 +10,13 @@ from machine import Pin
 
 # Set how many LEDs you have
 NUM_LEDS = 50
+DADO_START = 3
+DADO_END = 20
+BOX_START = 21
+BOX_END = 50
 
 def random_rgb():
-    return random.randint(30,200), random.randint(30,200), random.randint(30,200)
+    return random.randint(30,250), random.randint(30,250), random.randint(30,250)
 
 def shiraz_walk():
     print('SHIRAZ WALK!')
@@ -21,13 +25,30 @@ def shiraz_walk():
         # light up the LEDs in order in the random colour
         r, g, b = random_rgb()
         for i in range(NUM_LEDS):
-            led_strip.set_rgb(i, r, g, b)
-            time.sleep(0.03)
+            # DADO
+            if ( i+DADO_START < DADO_END):
+                led_strip.set_rgb(i+DADO_START, r, g, b)
+            if ( i+BOX_START < BOX_END):
+                led_strip.set_rgb(i+BOX_START, r, g, b)
+            time.sleep(0.1)
+            if ( i+DADO_START < DADO_END):
+                led_strip.set_rgb(i+DADO_START, 0, 0, 0)
+            if ( i+BOX_START < BOX_END):
+                led_strip.set_rgb(i+BOX_START, 0, 0, 0)
+
+
         # now walk back in a different colour
         r, g, b = random_rgb()
         for i in reversed(range(NUM_LEDS)):
-            led_strip.set_rgb(i, r, g, b)
-            time.sleep(0.03)
+            if ( i+DADO_START < DADO_END):
+                led_strip.set_rgb(i+DADO_START, r, g, b)
+            if ( i+BOX_START < BOX_END):
+                led_strip.set_rgb(i+BOX_START, r, g, b)
+            time.sleep(0.1)
+            if ( i+DADO_START < DADO_END):
+                led_strip.set_rgb(i+DADO_START, 0, 0, 0)
+            if ( i+BOX_START < BOX_END):
+                led_strip.set_rgb(i+BOX_START, 0, 0, 0)
 
 # set up the Pico W's onboard LED
 pico_led = Pin('LED', Pin.OUT)
